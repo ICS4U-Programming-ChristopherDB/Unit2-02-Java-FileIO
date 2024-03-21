76406a09c0aada846f1d7ef1b04d68c8436e8020
Christopher <92882782+Krostofer@users.noreply.github.com>
2024-03-21 10:40:36 -0400

    Initial commit

---
+++ com/example/FileIO.java
@@ -0,0 +1,28 @@
+
+package com.example;
+
+/**
+ * Change me.
+ *
+ * @author Christopher Di Bert
+ * @version 1.0
+ * @since 2024-03-21
+ */
+
+// FileIO class
+public final class FileIO {
+
+    /** Private constructor to prevent instantiation. */
+    private FileIO() {
+        throw new UnsupportedOperationException("Cannot instantiate");
+    }
+
+    /**
+     * This is the main method.
+     *
+     * @param args Unused
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, world");
+    }
+}
